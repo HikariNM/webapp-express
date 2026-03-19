78515d34fb756ed3b630e5b1589d5cb441d339c6
HikariNM <nicole.hikari.mascellino@gmail.com>
2026-03-19 18:43:37 +0100

    first commit

---
+++ controllers/movieController.js
@@ -0,0 +1,47 @@
+const db = require('../data/db');
+
+function index(req, res) {
+    const sqlQuery = 'SELECT * FROM movies'
+
+    db.query(sqlQuery, (err, res) => {
+        if (err) {
+            return res.status(500).json({ error: "DB Error", message: "Error retrieving data from the database" });
+        }
+        res.json(res)
+    })
+}
+
+function show(req, res) {
+    const id = Number(req.params.id)
+    const sqlQuery = 'SELECT * FROM movies WHERE id = ?';
+    const relationsQuery = `
+    SELECT 
+    reviews.text
+    FROM movies
+    JOIN reviews
+    ON movies.id = reviews.movie_id
+    WHERE movies.id = ?;
+    `;
+    const paramsQuery = [id];
+
+
+    db.query(sqlQuery, paramsQuery, (err, row) => {
+        if (err) {
+            return res.status(500).json({ error: "DB Error", message: "Error retrieving data from the database" });
+        }
+        const movie = row[0];
+
+        db.query(relationsQuery, paramsQuery, (err, result) => {
+            if (err) {
+                console.error('errore 2')
+                return res.status(500).json({ error: "DB Error", message: "Error retrieving data from the database" });
+            }
+            movie.text = result.map(rec => rec.text);
+            // console.log(movie);
+            res.json(movie);
+        })
+    })
+
+}
+
+module.exports = { index, show };
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const movieRouter = require('./routers/movieRouter');
+const notFound = require('./middlewares/notFound');
+const errorsHandler = require('./middlewares/errorsHandler');
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome!');
+})
+
+app.use('/movies', movieRouter);
+
+app.use(notFound);
+app.use(errorsHandler);
+
+
+app.listen(port, () => {
+    console.log(`Express listening at http://localhost:${port}/`);
+});
